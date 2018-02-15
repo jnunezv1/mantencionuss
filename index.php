fd95f9e6c971d40bfba239d5b61298e80a6f7c1b
jnunezv1 <jnunezv1@ussmail.cl>
2018-02-15 19:13:32 -0300

    index

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>USS</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Animation CSS -->
+    <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+<body id="page-top" class="landing-page">
+<div id="inSlider" class="carousel carousel-fade" data-ride="carousel">
+
+	<div class="video-container">
+	   <video autoplay loop muted>
+		     <source src="/assets/video/video-uss.mp4" type="video/mp4">
+		     Your browser does not support the video tag.
+	   </video>
+	</div>
+
+
+</div>
+
+
+<section id="features" class="container services">
+	<div class="row">
+        <div class="col-lg-6 text-center text-success">
+            <img src="logo.svg" class="full-responsive">
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-sm-5">
+        	<div id="box-1" class="widget navy-bg">
+	            <h1><i class="fa fa-paint-brush"></i> Sitio en Mantención</h1>
+	        </div>
+        </div>
+    </div>
+
+    <!-- <div class="row">
+        <div class="col-sm-5">
+        	<div id="box-1" class="widget navy-bg">
+	            <h2>Sitio en Mantención</h2>
+	        </div>
+        </div>
+    </div> -->
+    
+    <div class="row">
+    	<div class="col-lg-4">
+            <div class="widget style1 yellow-bg">
+                <div class="row">
+                    <div class="col-xs-12">
+                    	<div class="row">
+	                         <div class="col-xs-2"><a href="https://www.facebook.com/ComunidadUSS"><i class="fa fa-facebook-square fa-3x"></i></a></div>
+	                         <div class="col-xs-2"><a href="https://www.instagram.com/usansebastian/"><i class="fa fa-instagram fa-3x"></i></a></div>
+	                         <div class="col-xs-2"><a href="https://twitter.com/@USanSebastian"><i class="fa fa-twitter-square fa-3x"></i></a></div>
+	                         <div class="col-xs-2"><a href="https://www.youtube.com/channel/UC22O4-WupSB0XfaiQnMzK3g"><i class="fa fa-youtube fa-3x"></i></a></div>
+	                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+</section>
+
+<!-- Mainly scripts -->
+<script src="assets/js/jquery-3.1.1.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+
+</body>
+</html>
